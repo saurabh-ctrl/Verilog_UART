8ec8bd78fde237b4c7be93db50e8225cf20e118d
saurabh-ctrl <61474697+saurabh-ctrl@users.noreply.github.com>
2020-07-09 11:52:51 +0530

    Update uart_Top.v

--- uart_Top.v
+++ uart_Top.v
@@ -11,11 +11,11 @@
 
 module uart_top #(parameter CLK_CY_PER_BIT = 87)
 	(
-		input 			i_clk,
-		input 			i_Tx_Dv,
-		input [7:0]		i_Tx_Byte,
+		input 		i_clk,
+		input 		i_Tx_Dv,
+		input  [7:0]	i_Tx_Byte,
 		output [7:0]	o_Rx_Byte,
-		output 			o_Rx_Dv
+		output 		o_Rx_Dv
 	);
 	
 	// Defining the Internal connection:
@@ -28,13 +28,13 @@ module uart_top #(parameter CLK_CY_PER_BIT = 87)
 	
 	// 1. UART Transmitter:
 	uart_tx #(.CLK_CY_PER_BIT(CLK_CY_PER_BIT)) UART_TX_INST
-    (.i_clk(i_clk),
-     .i_Tx_Dv(i_Tx_Dv),
-     .i_Tx_Byte(i_Tx_Byte),
-     .o_Tx_Active(o_Tx_Active),
-     .o_Tx_Serial(io_Serial),
-     .o_Tx_Done(o_Tx_Done)
-    );
+	    (.i_clk(i_clk),
+	     .i_Tx_Dv(i_Tx_Dv),
+	     .i_Tx_Byte(i_Tx_Byte),
+	     .o_Tx_Active(o_Tx_Active),
+	     .o_Tx_Serial(io_Serial),
+	     .o_Tx_Done(o_Tx_Done)
+	    );
 	
 	// 2. UART Receiver:
 	uart_rx #(.CLK_CY_PER_BIT(CLK_CY_PER_BIT)) UART_RX_INST
